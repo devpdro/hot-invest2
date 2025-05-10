31a845d9a52a20378b11c3333e7a6cdcc68da582
Victor Pedro <victorh.pedr@gmail.com>
2025-05-10 02:58:27 -0300

    UPDATE

--- src/app/abrir-conta/page.tsx
+++ src/app/abrir-conta/page.tsx
@@ -1,9 +1,11 @@
 'use client'
+
 import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { makeOpenAccount } from 'src/main/factories/pages';
+import { Suspense } from 'react';
 
-export default function AbrirContaPage() {
+function OpenAccountWithSearchParams() {
     const searchParams = useSearchParams();
     const router = useRouter();
     const cpf = searchParams.get('cpf') || '';
@@ -15,4 +17,12 @@ export default function AbrirContaPage() {
     }, [cpf, router]);
 
     return makeOpenAccount({ cpf });
+}
+
+export default function OpenAccountPage() {
+    return (
+        <Suspense fallback={null}>
+            <OpenAccountWithSearchParams />
+        </Suspense>
+    );
 }
